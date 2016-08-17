12ca1361bffcf495f107bc1b6fb3e22faffb90b5
Anne Sophie Cadre <anne-sophie.cadre@orange.fr>
2016-08-17 10:23:04 +1000

    create a android project that is not blank

--- BrisNav/BrisNav/MainActivity.cs
+++ BrisNav/BrisNav/MainActivity.cs
@@ -1,25 +1,28 @@
-﻿using System;
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
+﻿using Android.App;
 using Android.Widget;
 using Android.OS;
 
 namespace BrisNav
 {
-    [Activity(Label = "BrisNav", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
-    {
+	[Activity(Label = "BrisNav", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
 
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-            
-        }
-    }
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+		}
+	}
 }
+
 
